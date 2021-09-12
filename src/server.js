d11858e937cb25e263a76084ffe1d9ac2a6fb73e
hellomac87 <hellomac87@gmail.com>
2021-09-12 23:37:18 +0900

    front-end configuration

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
-console.log('hello doom');
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
-app.listen(3000);
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.render('home'));
+
+const handleListen = () => console.log('Listenning on http://localhost:3000');
+
+app.listen(3000, handleListen);
